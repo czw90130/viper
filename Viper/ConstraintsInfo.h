b02bcb1b2c1bd1e962f89bc88f7dffed2ab8ceb9
chuzhiwei <chuzhiwei@suanier.com>
2023-08-19 22:28:10 +0800

    chroe: 更新约束头文件注释

--- Viper/ConstraintsInfo.h
+++ Viper/ConstraintsInfo.h
@@ -13,24 +13,63 @@
 #pragma once
 #include "Common.h"
 
+/**
+ * @brief 约束信息结构体
+ * 
+ * 该结构体用于存储和管理场景中不同类型的约束信息。
+ * 约束 (Constraint): 在物理模拟中，约束定义了某些物体或物体部分之间的相对关系。这些关系可以是固定的，也可以是可变的。约束通常用于确保模拟在某些条件下保持稳定，例如确保物体不会穿透其他物体。
+ * 投影 (Projection): 在约束求解的上下文中，投影通常指的是将物体从违反约束的位置移动到满足约束的位置的过程。例如，如果两个物体被约束为不得接触，但由于某种原因它们接触了，那么它们的位置会被“投影”回到不接触的位置。
+ * Lambda: 在某些物理模拟算法中，尤其是那些使用拉格朗日乘子的方法来解决约束的算法中，lambda代表了约束的强度或权重。实际上，它是约束修正的一个因子，决定了应用于物体以满足约束的力的大小。
+ * 投影偏移量 (Projection Offset): 这可能是指用于存储每种约束类型投影数据的数组中的起始位置。在具有多种约束类型的复杂模拟中，每种约束可能需要不同数量的数据来描述。投影偏移量可以帮助我们快速找到某种约束类型的投影数据的开始位置。
+ * Lambda偏移量 (Lambda Offset): 与投影偏移量类似，这可能是指用于存储每种约束类型的lambda数据的数组中的起始位置。
+ */
 struct ConstraintsInfo {
+    /**
+     * @brief 添加一个新的约束类型的信息。
+     * 
+     * 该函数允许用户添加关于新约束类型的信息，如其名称、数量、与其关联的投影和lambda数量等。
+     * 每种约束类型的信息都会存储在各自的数组中，并用于后续的模拟和计算。
+     * 
+     * @param name 约束的名称。每种约束都应该有一个独特的名称。
+     * @param count 当前约束类型的数量。
+     * @param projections_per_constraint 每个约束的投影数量。这表示为了满足这种类型的约束，可能需要进行多少次投影。
+     * @param lambda_per_constraint 每个约束的lambda数量。lambda是用于约束修正的因子，表示每种约束的强度或权重。
+     */
     void add(const std::string &name, int count, int projections_per_constraint,
-             int lambda_per_constraint) {
+            int lambda_per_constraint) {
+        // 确保同名的约束还没有被添加过
         assert(index.find(name) == index.end());
+
+        // 获取np的大小作为新约束类型的索引值
         int i = np.size();
+
+        // 将约束名称与其索引值相关联
         index[name] = i;
+
+        // 如果这是第一次添加约束，设置初始的投影和lambda偏移量为0
         if (i == 0) {
             o.push_back(0);
             ol.push_back(0);
         } else {
+            // 否则，根据前一个约束的偏移量和数量，计算新的偏移量
             o.push_back(o.back() + np.back());
             ol.push_back(ol.back() + nl.back());
         }
+
+        // 存储当前约束类型的数量
         nc.push_back(count);
+        // 存储当前约束类型的总投影数量
         np.push_back(count * projections_per_constraint);
+        // 存储当前约束类型的总lambda数量
         nl.push_back(count * lambda_per_constraint);
     }
 
+
+    /**
+     * @brief 获取约束的总数量
+     * 
+     * @return 约束的总数量
+     */
     int get_nc() {
         int n = 0;
         for (int i = 0; i < nc.size(); i++) {
@@ -39,18 +78,33 @@ struct ConstraintsInfo {
         return n;
     }
 
+    /**
+     * @brief 获取投影的总数量
+     * 
+     * @return 投影的总数量
+     */
     int get_np() {
         if (np.size() == 0)
             return 0;
         return o.back() + np.back();
     }
 
+    /**
+     * @brief 获取lambda的总数量
+     * 
+     * @return lambda的总数量
+     */
     int get_nl() {
         if (np.size() == 0)
             return 0;
         return ol.back() + nl.back();
     }
 
+    /**
+     * @brief 获取每个约束类型的投影偏移量
+     * 
+     * @return 约束类型与其对应的投影偏移量的映射
+     */
     std::map<std::string, int> get_o() {
         std::map<std::string, int> m;
         for (auto const &x : index)
@@ -58,6 +112,11 @@ struct ConstraintsInfo {
         return m;
     }
 
+    /**
+     * @brief 获取每个约束类型的lambda偏移量
+     * 
+     * @return 约束类型与其对应的lambda偏移量的映射
+     */
     std::map<std::string, int> get_ol() {
         std::map<std::string, int> m;
         for (auto const &x : index)
@@ -67,9 +126,9 @@ struct ConstraintsInfo {
 
   private:
     std::map<std::string, int> index;
-    std::vector<int> nc; // number of constraints per type
-    std::vector<int> np; // number of projections per type
-    std::vector<int> nl; // number of lambdas per type
-    std::vector<int> o;  // projection offset per type
-    std::vector<int> ol; // lambda offset per type
+    std::vector<int> nc; ///< 约束名到其在数组中的索引的映射 number of constraints per type
+    std::vector<int> np; ///< 每种约束类型的数量 number of projections per type
+    std::vector<int> nl; ///< 每种约束类型的投影数量 number of lambdas per type
+    std::vector<int> o;  ///< 每种约束类型的lambda数量 projection offset per type
+    std::vector<int> ol; ///< 每种约束类型的lambda偏移量 lambda offset per type
 };
--- Viper/CudaConstraints.h
+++ Viper/CudaConstraints.h
@@ -13,12 +13,27 @@
 #pragma once
 #include "State.h"
 
+/**
+ * @file CudaConstraints.h
+ * @brief 定义各种物理约束结构的文件。
+ * 
+ * 这个文件包含多种物理模拟中使用的约束结构，包括距离约束、蒙皮约束、体积约束等。
+ */
+
 namespace viper {
 
+/**
+ * @brief 约束的基础结构。
+ * 
+ * 所有约束都从这个基础结构继承。
+ */
 struct ConstraintBase {
-    bool enabled = true;
+    bool enabled = true; ///< 标记此约束是否启用。
 };
 
+/**
+ * @brief 距离约束。
+ */
 struct C_distance : public ConstraintBase {
     C_distance() = default;
     C_distance(int a, int b, float restDistance, float compliance = 0.f)
@@ -29,6 +44,9 @@ struct C_distance : public ConstraintBase {
     float compliance; // inverse stiffness
 };
 
+/**
+ * @brief 最大距离约束。
+ */
 struct C_distancemax : public ConstraintBase {
     C_distancemax() = default;
     C_distancemax(int a, int b, float max_distance)
@@ -38,158 +56,270 @@ struct C_distancemax : public ConstraintBase {
     float max_distance;
 };
 
+/**
+ * @brief 皮肤约束
+ */
 struct C_skinning : public ConstraintBase {
     C_skinning() = default;
     C_skinning(int i, int t0, int t1, float w0, float w1)
         : i(i), t0(t0), t1(t1), w0(w0), w1(w1) {}
-    int i;
-    int t0;
-    int t1;
-    float w0;
-    float w1;
+    int i; ///< 皮肤索引
+    int t0; ///< 初始时间
+    int t1; ///< 结束时间
+    float w0; ///< 初始权重
+    float w1; ///< 结束权重
 };
 
+/**
+ * @brief 体积约束
+ */
 struct C_volume : public ConstraintBase {
+    // 默认构造函数
     C_volume() = default;
+    /**
+     * @brief 构造函数用于设置粒子索引、参考体积和顺从度
+     * @param a 粒子索引a
+     * @param b 粒子索引b
+     * @param Vr 参考体积
+     * @param compliance 顺从度，默认为0.0
+     */
     C_volume(int a, int b, float Vr, float compliance = 0.0)
         : a(a), b(b), Vr(Vr), compliance(compliance) {}
+
+    /**
+     * @brief 构造函数用于根据模拟状态设置粒子索引和顺从度，并计算参考体积
+     * @param a 粒子索引a
+     * @param b 粒子索引b
+     * @param state 当前的模拟状态，其中包含粒子的位置和半径
+     * @param compliance 顺从度，默认为0.0
+     */
     C_volume(int a, int b, const SimulationState &state, float compliance = 0.0)
         : a(a), b(b), compliance(compliance) {
+        // 获取粒子a和b的半径
         float ra = state.r[a];
         float rb = state.r[b];
+        
+        // 计算粒子a和b之间的距离
         float d = (state.x[a] - state.x[b]).norm();
+        
+        // 如果距离大于一个非常小的阈值
         if (d > 1e-7f) {
+            // 计算两个粒子的距离和半径之间的关系
             float e = (rb - ra) / d;
+            
+            // 计算L，一个与距离和半径有关的参数
             float L = d + (ra - rb) * e;
+            
+            // 根据上述参数，使用公式计算参考体积
             Vr = M_PIf / 3.0f *
                  ((ra * ra * ra - rb * rb * rb) * (e * e * e - 3.f * e) +
                   L * (1.f - e * e) * (ra * ra + ra * rb + rb * rb));
-        } else
+        } else {
+            // 若粒子距离非常接近，则参考体积设置为0
             Vr = 0.f;
+        }
     }
 
-    int a;
-    int b;
-    float Vr;
-    float compliance;
+    int a; ///< 粒子索引a
+    int b; ///< 粒子索引b
+    float Vr; ///< 参考体积
+    float compliance; ///< 顺从度
 };
 
+/**
+ * @brief 体积约束2
+ * 
+ * 该结构体表示一个体积约束，它用于确保模拟中某些三维结构的体积保持恒定。
+ * 此版本的体积约束涉及三个实体：两个粒子和一个条索，可能用于模拟柔性管状结构或其他类似的物体。
+ */
 struct C_volume2 : public ConstraintBase {
+    
+    // 默认构造函数
     C_volume2() = default;
+
+    /**
+     * @brief 构造函数，用于根据模拟状态和粒子索引初始化体积约束2
+     * 
+     * @param a 第一个粒子的索引
+     * @param b 第二个粒子的索引
+     * @param c 条索引，可能代表一个连接粒子a和粒子b的柔性管或线条
+     * @param state 当前的模拟状态，其中包含粒子的位置和半径信息
+     * @param compliance 顺从度，默认为0.0
+     */
     C_volume2(int a, int b, int c, const SimulationState &state,
               float compliance = 0.0)
         : a(a), b(b), c(c), compliance(compliance) {
+        // 计算粒子a和粒子b之间的初始距离或长度
         l0 = (state.x[a] - state.x[b]).norm();
+        // 计算两个粒子的平均半径作为初始半径
         r0 = 0.5f * (state.r[a] + state.r[b]);
     }
 
-    int a;
-    int b;
-    int c;
-    float compliance;
-    float l0;
-    float r0;
+    int a; ///< 粒子索引a
+    int b; ///< 粒子索引b
+    int c; ///< 条索引。代表一个连接粒子a和粒子b的柔性管或线条的索引。
+    float compliance; ///< 顺从度，表示结构的柔性或硬度，值越大越柔软。
+    float l0; ///< 初始长度。表示粒子a和粒子b在没有受到外部影响时的距离。
+    float r0; ///< 初始半径。表示粒子a和粒子b的平均半径。
 };
 
+/**
+ * @brief 弯曲约束
+ * 
+ * 该结构体表示一个弯曲约束，主要目的是维持两个粒子之间的旋转关系，从而产生弯曲效果。
+ */
 struct C_bend : public ConstraintBase {
+    
+    // 默认构造函数
     C_bend() = default;
+
+    /**
+     * @brief 构造函数，用于根据模拟状态和粒子索引初始化弯曲约束
+     * 
+     * @param a 第一个粒子的索引
+     * @param b 第二个粒子的索引
+     * @param state 当前的模拟状态，其中包含粒子的四元数（旋转信息）
+     * @param compliance 顺从度，默认为0.0
+     */
     C_bend(int a, int b, const SimulationState &state, float compliance = 0.f)
         : a(a), b(b), compliance(compliance) {
+        // 计算休息位置的Darboux向量，表示两个粒子在没有外部力作用时的旋转关系
+        // 这里使用第一个粒子的四元数的共轭与第二个粒子的四元数相乘得到
         darbouxRest = state.q[a].conjugate() * state.q[b];
     }
 
-    int a;
-    int b;
-    Quaternion darbouxRest;
-    float compliance;
+    int a; ///< 粒子索引a
+    int b; ///< 粒子索引b
+    Quaternion darbouxRest; ///< 休息位置的Darboux向量。这个向量描述了在没有外部力作用时，两个粒子之间的旋转关系。
+    float compliance; ///< 顺从度，表示结构的柔性或硬度，值越大越柔软
 };
 
+/**
+ * @brief 拉伸约束
+ */
 struct C_stretch : public ConstraintBase {
     C_stretch() = default;
     C_stretch(int a, int b, int c, float L, float compliance = 0.f)
         : a(a), b(b), c(c), L(L), compliance(compliance) {}
 
-    int a;   // particle id
-    int b;   // partile id
-    int c;   // pill id
-    float L; // rest length
-    float compliance;
+    int a;   ///< 粒子索引a particle id
+    int b;   ///< 粒子索引b partile id
+    int c;   ///< 条索引 pill id
+    float L; ///< 休息长度 rest length
+    float compliance; ///< 顺从度
 };
 
+/**
+ * @brief 粒子-粒子碰撞约束
+ */
 struct C_collpp : public ConstraintBase {
-    int a;
-    int b;
+    int a; ///< 粒子索引a
+    int b; ///< 粒子索引b
 };
 
+/**
+ * @brief 碰撞约束
+ */
 struct C_collision : public ConstraintBase {
-    Vec2i a;
-    Vec2i b;
+    Vec2i a; ///< 粒子对a
+    Vec2i b; ///< 粒子对b
 };
 
+/**
+ * @brief 半径约束
+ */
 struct C_radius : public ConstraintBase {
     C_radius() = default;
     C_radius(int a, float r, float compliance = 0.0)
         : a(a), r(r), compliance(compliance) {}
-    int a;
-    float r;
-    float compliance;
+    int a; ///< 粒子索引
+    float r; ///< 半径
+    float compliance; ///< 顺从度
 };
 
+/**
+ * @brief 双拉普拉斯约束
+ *
+ * 该结构体描述了双拉普拉斯约束，通常用于模拟物体的表面或体积的弯曲、褶皱等效果。
+ * 它使用了一个粒子索引数组以及相应的权重数组来定义约束的影响。
+ */
 struct C_bilap : public ConstraintBase {
+    /// @brief 默认构造函数
     C_bilap() = default;
+
+    /**
+     * @brief 参数化构造函数
+     *
+     * @param A           由粒子索引构成的向量
+     * @param i           当前粒子的索引
+     * @param compliance  顺从度
+     *
+     * 根据给定的粒子索引向量和当前粒子索引，初始化权重数组和粒子索引数组。
+     * 权重数组和粒子索引数组的值根据当前粒子索引i的位置进行不同的初始化。
+     */
     C_bilap(const std::vector<int> &A, int i, float compliance = 0.0)
         : compliance(compliance) {
         int n = A.size();
         if (i == 0) {
+            // 当i为0时，当前粒子是向量的第一个元素
             ids[0] = A[i];
             ids[1] = A[i];
             ids[2] = A[i];
             ids[3] = A[i + 1];
             ids[4] = A[i + 2];
+            // 为这种情况分配特定的权重
             w[0] = 0.f;
             w[1] = 0.f;
             w[2] = 2.f;
             w[3] = -3.f;
             w[4] = 1.f;
         } else if (i == 1) {
+            // 当i为1时，当前粒子是向量的第二个元素
             ids[0] = A[i];
             ids[1] = A[i - 1];
             ids[2] = A[i];
             ids[3] = A[i + 1];
             ids[4] = A[i + 2];
+            // 为这种情况分配特定的权重
             w[0] = 0.f;
             w[1] = -3.f;
             w[2] = 6.f;
             w[3] = -4.f;
             w[4] = 1.f;
         } else if (i > 1 && i < n - 2) {
+            // 当i在第三个和倒数第三个元素之间时
             ids[0] = A[i - 2];
             ids[1] = A[i - 1];
             ids[2] = A[i];
             ids[3] = A[i + 1];
             ids[4] = A[i + 2];
+            // 为这种情况分配特定的权重
             w[0] = 1.f;
             w[1] = -4.f;
             w[2] = 6.f;
             w[3] = -4.f;
             w[4] = 1.f;
         } else if (i == n - 2) {
+            // 当i为倒数第二个元素时
             ids[0] = A[i - 2];
             ids[1] = A[i - 1];
             ids[2] = A[i];
             ids[3] = A[i + 1];
             ids[4] = A[i];
+            // 为这种情况分配特定的权重
             w[0] = 1.f;
             w[1] = -4.f;
             w[2] = 6.f;
             w[3] = -3.f;
             w[4] = 0.f;
         } else if (i == n - 1) {
+            // 当i为向量的最后一个元素时
             ids[0] = A[i - 2];
             ids[1] = A[i - 1];
             ids[2] = A[i];
             ids[3] = A[i];
             ids[4] = A[i];
+            // 为这种情况分配特定的权重
             w[0] = 1.f;
             w[1] = -3.f;
             w[2] = 2.f;
@@ -198,84 +328,138 @@ struct C_bilap : public ConstraintBase {
         }
     }
 
-    int ids[5];
-    float w[5];
-    float compliance;
+    int ids[5];       ///< 粒子索引数组，用于存储与当前粒子相关的其他粒子的索引
+    float w[5];       ///< 权重数组，存储与当前粒子相关的其他粒子的权重，用于约束计算
+    float compliance; ///< 顺从度，用于调整约束的强度，较高的顺从度表示约束较为柔软
 };
 
 #define SHAPE_MATCHING_MAX 11
 
+/**
+ * @brief 形状匹配约束
+ */
 struct C_shape : public ConstraintBase {
+    
+    // 默认构造函数
     C_shape() = default;
+
+    /**
+     * @brief 构造函数用于根据模拟状态初始化形状匹配约束
+     * @param ids 由粒子索引组成的向量，这些粒子需要匹配到某一特定形状
+     * @param state 当前的模拟状态，其中包含粒子的位置和半径
+     * @param compliance 顺从度，默认为0.0
+     */
     C_shape(const std::vector<int> &ids, const SimulationState &state,
             float compliance = 0.0)
-        : q(Quaternion::Identity()), K(1.f), compliance(compliance) {
+        : q(Quaternion::Identity()), // 设置默认旋转量为单位四元数（无旋转）
+          K(1.f),                    // 默认的刚性系数为1
+          compliance(compliance) {
+        
+        // 获取粒子数量
         n = ids.size();
+        
+        // 断言确保粒子数量不超过预设的最大值
         assert(n <= SHAPE_MATCHING_MAX);
+        
+        // 断言确保至少有两个粒子
         assert(n > 1);
 
+        // 计算粒子的中心点
         Vec3 center = Vec3::Zero();
         for (int i = 0; i < n; i++)
             center += state.x[ids[i]];
         center /= (float)n;
 
+        // 初始化粒子索引数组和参考位置数组
         for (int i = 0; i < SHAPE_MATCHING_MAX; i++) {
             if (i < n) {
-                id[i] = ids[i];
-                xp[i] = state.x[ids[i]] - center;
+                id[i] = ids[i];                // 设置粒子索引
+                xp[i] = state.x[ids[i]] - center; // 设置粒子相对于中心的位置
             } else {
-                id[i] = 0;
-                xp[i] = Vec3::Zero();
+                id[i] = 0;                     // 超出粒子数量的索引默认为0
+                xp[i] = Vec3::Zero();          // 超出粒子数量的位置为0向量
             }
         }
 
+        // 计算平均半径
         r = 0.f;
         for (int i = 0; i < n; i++)
             r += state.r[id[i]];
         r /= (float)n;
     }
 
-    int n;
-    int id[SHAPE_MATCHING_MAX];
-    Vec3 xp[SHAPE_MATCHING_MAX];
-    Quaternion q;
-    float K;
-    float r;
-    float compliance;
+    int n; ///< 粒子数量
+    int id[SHAPE_MATCHING_MAX]; ///< 粒子索引数组
+    Vec3 xp[SHAPE_MATCHING_MAX]; ///< 参考位置数组，代表了在没有外部力作用下粒子应该在的位置
+    Quaternion q; ///< 旋转量，用于使粒子旋转到匹配形状
+    float K; ///< 刚性系数，控制形状匹配的强度
+    float r; ///< 半径，代表了参与形状匹配的粒子的平均半径
+    float compliance; ///< 顺从度，表示结构的柔性或硬度，值越大越柔软
 };
 
+
+/**
+ * @brief 形状匹配约束2
+ * 
+ * 该结构体用于确保模拟中某些三维结构的形状保持恒定，通过限制它们的旋转和变形。
+ * 此版本的形状匹配约束涉及一个粒子数组以及与之对应的两个旋转量数组。
+ */
 struct C_shape2 : public ConstraintBase {
+    
+    ///< 默认构造函数
     C_shape2() = default;
+
+    /**
+     * @brief 构造函数，用于初始化形状匹配约束2
+     * 
+     * @param ids 粒子索引的数组
+     * @param qas 与粒子相对应的旋转量a的数组
+     * @param qbs 与粒子相对应的旋转量b的数组
+     * @param state 当前的模拟状态
+     */
     C_shape2(const std::vector<int> &ids, const std::vector<int> &qas,
              const std::vector<int> &qbs, const SimulationState &state) {
+        // 获取粒子的数量
         n = ids.size();
+        
+        // 确保粒子数量不超过设定的最大值
         assert(n <= SHAPE_MATCHING_MAX);
+        // 确保至少有两个粒子
         assert(n > 1);
+        // 确保粒子数量与旋转量数组的大小匹配
         assert(n == qas.size() && n == qbs.size());
 
+        // 初始化粒子索引以及它们的旋转量
         for (int i = 0; i < n; i++) {
             id[i] = ids[i];
             qa[i] = qas[i];
             qb[i] = qbs[i];
         }
 
+        // 设置整体的初始旋转量为单位四元数
         q = Quaternion::Identity();
     }
 
-    int n;
-    int id[SHAPE_MATCHING_MAX];
-    int qa[SHAPE_MATCHING_MAX];
-    int qb[SHAPE_MATCHING_MAX];
-    Quaternion q;
+    int n; ///< 粒子数量。表示受到此约束影响的粒子总数。
+    int id[SHAPE_MATCHING_MAX]; ///< 粒子索引数组。存储受到此约束影响的所有粒子的索引。
+    int qa[SHAPE_MATCHING_MAX]; ///< 旋转量a数组。存储每个粒子的旋转量a。
+    int qb[SHAPE_MATCHING_MAX]; ///< 旋转量b数组。存储每个粒子的旋转量b。
+    Quaternion q; ///< 整体旋转量。表示整体受到的旋转，用于调整粒子的相对位置。
 };
 
+/**
+ * @brief 触摸约束
+ */
 struct C_touch : public ConstraintBase {
-    C_touch() = default;
-    C_touch(int a, int b) : a(a), b(b) {}
-    int a;
-    int b;
+    C_touch() = default; ///< 默认构造函数
+    C_touch(int a, int b) : a(a), b(b) {} ///< 带参数构造函数
+    int a; ///< 粒子索引a
+    int b; ///< 粒子索引b
 };
 
+/**
+ * @brief 包含所有约束类型的集合。
+ */
 struct ConstraintsCPU {
     std::vector<C_distance> distance;
     std::vector<C_distancemax> distancemax;
