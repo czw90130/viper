fa0f2f365108e6b2e844163f2e717c623fd8ca86
chuzhiwei <chuzhiwei@suanier.com>
2023-08-21 21:54:33 +0800

    chore: 增加 demo 注释

--- Demo/OctopusComponent.h
+++ Demo/OctopusComponent.h
@@ -34,14 +34,23 @@
 
 namespace OpenGP {
 
+/**
+ * @struct OctopusData
+ * @brief 数据结构，用于保存章鱼的各种约束条件。
+ */
 struct OctopusData {
-    std::vector<int> radius_constraints;
-    std::vector<int> volume_constraints;
-    std::vector<int> stretch_constraints;
-    std::vector<int> distance_constraints;
-    std::vector<int> bend_constraints;
-    std::vector<int> cannonball_constraints;
+    std::vector<int> radius_constraints; ///< 半径约束的索引列表
+    std::vector<int> volume_constraints; ///< 体积约束的索引列表
+    std::vector<int> stretch_constraints; ///< 拉伸约束的索引列表
+    std::vector<int> distance_constraints; ///< 距离约束的索引列表
+    std::vector<int> bend_constraints; ///< 弯曲约束的索引列表
+    std::vector<int> cannonball_constraints; ///< 炮弹约束的索引列表
 
+    /**
+     * @brief 设置指定约束的启用状态
+     * @param enabled 启用或禁用的标志
+     * @param scene 引用的场景对象
+     */
     void set_enabled(bool enabled, viper::Scene &scene) {
         for (auto i : radius_constraints)
             scene.constraints.radius[i].enabled = enabled;
@@ -55,56 +64,70 @@ struct OctopusData {
             scene.constraints.bend[i].enabled = enabled;
     }
 
+    /**
+     * @brief 设置炮弹约束的启用状态
+     * @param enabled 启用或禁用的标志
+     * @param scene 引用的场景对象
+     */
     void set_cannonball_enabled(bool enabled, viper::Scene &scene) {
         for (auto i : cannonball_constraints)
             scene.constraints.distance[i].enabled = enabled;
     }
 };
 
+/**
+ * @class OctopusComponent
+ * @brief 章鱼组件类，用于描述章鱼的各种属性和行为。
+ */
 class OctopusComponent : public Component {
   public:
-    static viper::Scene *v_scene;
+    static viper::Scene *v_scene; ///< 静态场景指针
 
-    const int n_cows = 3;
-    int n_active = 3;
+    const int n_cows = 3; ///< 牛的数量
+    int n_active = 3; ///< 活跃的数量
 
-    int scene_index = 0;
+    int scene_index = 0; ///< 场景索引
 
-    bool cannonballs_active = false;
+    bool cannonballs_active = false; ///< 炮弹是否活跃的标志
 
-    WorldRenderComponent *render_comp;
-    SurfaceMeshRenderer *renderer;
-    WorldRenderComponent *sphere_render_comp;
-    SphereMeshRenderer *sphere_renderer;
-    WorldRenderComponent *tsphere_render_comp;
-    SphereMeshRenderer *tsphere_renderer;
-    WorldRenderComponent *cannonball_render_comp;
-    SphereMeshRenderer *cannonball_renderer;
-    WorldRenderComponent *pillar_render_comp;
-    SphereMeshRenderer *pillar_renderer;
+    WorldRenderComponent *render_comp; ///< 渲染组件指针
+    SurfaceMeshRenderer *renderer; ///< 表面网格渲染器指针
+    WorldRenderComponent *sphere_render_comp; ///< 球体渲染组件指针
+    SphereMeshRenderer *sphere_renderer; ///< 球体渲染器指针
+    WorldRenderComponent *tsphere_render_comp; ///< tsphere渲染组件指针
+    SphereMeshRenderer *tsphere_renderer; ///< tsphere渲染器指针
+    WorldRenderComponent *cannonball_render_comp; ///< 炮弹渲染组件指针
+    SphereMeshRenderer *cannonball_renderer; ///< 炮弹渲染器指针
+    WorldRenderComponent *pillar_render_comp; ///< 柱子渲染组件指针
+    SphereMeshRenderer *pillar_renderer; ///< 柱子渲染器指针
 
-    std::vector<std::vector<int>> v_ids, p_ids;
-    std::vector<int> cannonball_ids;
-    std::vector<int> pillar_ids;
-    std::vector<OctopusData> cow_data;
+    std::vector<std::vector<int>> v_ids, p_ids; ///< v和p的索引列表
+    std::vector<int> cannonball_ids; ///< 炮弹的索引列表
+    std::vector<int> pillar_ids; ///< 柱子的索引列表
+    std::vector<OctopusData> cow_data; ///< 牛的数据列表
 
-    SurfaceMesh mesh;
-    OpenGP::SphereMesh smesh, cannonball_smesh;
+    SurfaceMesh mesh; ///< 表面网格对象
+    OpenGP::SphereMesh smesh, cannonball_smesh; ///< 球体网格对象和炮弹网格对象
 
-    std::vector<Vec4> spheres;
-    std::vector<Vec2i> all_pills;
-    std::vector<int> control_pills;
-    std::vector<float> compliances;
-    std::vector<float> masses;
+    std::vector<Vec4> spheres; ///< 球体列表
+    std::vector<Vec2i> all_pills; ///< 所有药丸的列表
+    std::vector<int> control_pills; ///< 控制药丸的列表
+    std::vector<float> compliances; ///< 合规性列表
+    std::vector<float> masses; ///< 质量列表
 
-    std::vector<std::vector<Mat4x4>> init_transforms;
+    std::vector<std::vector<Mat4x4>> init_transforms; ///< 初始变换矩阵列表
 
-    using V = OpenGP::SphereMesh::Vertex;
+    using V = OpenGP::SphereMesh::Vertex; ///< 定义Vertex的类型别名为V
 
+    /**
+    * @brief 初始化函数，进行渲染组件的设置
+    */
     void init() {
+        /// 获取WorldRenderComponent并设置渲染器为SurfaceMeshRenderer
         render_comp = &(require<WorldRenderComponent>());
         renderer = &(render_comp->set_renderer<SurfaceMeshRenderer>());
 
+        /// 设置章鱼材质的顶点、几何、片元着色器
         Material octomat(R"GLSL(
 
             flat out int gid;
@@ -264,30 +287,37 @@ class OctopusComponent : public Component {
             }
 
         )GLSL");
+
+        /// 设置材质的属性
         octomat.set_property("material", VIPER_TEXTURE);
         octomat.set_property("diffuse", 5);
         octomat.set_property("ao_map", 6);
         octomat.set_property("shadow_map", 7);
 
+        /// 应用材质并重建渲染器，设置可见性为真
         renderer->set_material(octomat);
         renderer->rebuild();
         render_comp->visible = true;
 
+        /// 为球体渲染组件设置渲染器为SphereMeshRenderer
         sphere_render_comp =
             &(get_scene().create_entity_with<WorldRenderComponent>());
         sphere_renderer =
             &(sphere_render_comp->set_renderer<SphereMeshRenderer>());
 
+        /// 设置球体渲染器的材质和属性
         sphere_renderer->set_material(octomat);
         sphere_renderer->get_material().set_property("material", VIPER_GOLD);
         sphere_renderer->rebuild();
         sphere_render_comp->visible = false;
 
+        /// 为tsphere渲染组件设置渲染器为SphereMeshRenderer
         tsphere_render_comp =
             &(get_scene().create_entity_with<WorldRenderComponent>());
         tsphere_renderer =
             &(tsphere_render_comp->set_renderer<SphereMeshRenderer>());
 
+        /// 设置tsphere渲染器的材质和属性
         tsphere_renderer->set_material(octomat);
         tsphere_renderer->no_spheres = true;
         tsphere_renderer->get_material().set_property("material",
@@ -295,46 +325,58 @@ class OctopusComponent : public Component {
         tsphere_renderer->rebuild();
         tsphere_render_comp->visible = false;
 
+        /// 为cannonball渲染组件设置渲染器为SphereMeshRenderer
         cannonball_render_comp =
             &(get_scene().create_entity_with<WorldRenderComponent>());
         cannonball_renderer =
             &(cannonball_render_comp->set_renderer<SphereMeshRenderer>());
 
+        /// 设置cannonball渲染器的材质和属性
         cannonball_renderer->set_material(octomat);
         cannonball_renderer->get_material().set_property("material",
                                                          VIPER_GREY);
         cannonball_renderer->rebuild();
 
+        /// 为pillar渲染组件设置渲染器为SphereMeshRenderer
         pillar_render_comp =
             &(get_scene().create_entity_with<WorldRenderComponent>());
         pillar_renderer =
             &(pillar_render_comp->set_renderer<SphereMeshRenderer>());
 
+        /// 设置pillar渲染器的材质和属性
         pillar_renderer->set_material(octomat);
         pillar_renderer->get_material().set_property("material", VIPER_GREY);
         pillar_renderer->rebuild();
 
+        // 对于一个5x5的网格（i和j分别从0到4）
         for (int i = 0; i < 5; ++i) {
             for (int j = 0; j < 5; ++j) {
 
+                // 定义一个半径为0.5的圆柱，其中c0和c1为两端的中心点
                 float r = 0.5;
                 Vec3 c0 = Vec3(4 * (i - 2), 0, 4 * (j - 2));
                 Vec3 c1 = Vec3(4 * (i - 2), 4, 4 * (j - 2));
 
+                // 在v_scene中添加两个固定的粒子，代表圆柱的两个端点
                 int id0 = v_scene->addParticle(c0, r, 1.0, true);
                 int id1 = v_scene->addParticle(c1, r, 1.0, true);
 
+                // 将这两个粒子的ID存储到pillar_ids向量中，以备后用
                 pillar_ids.push_back(id0);
                 pillar_ids.push_back(id1);
 
+                // 在v_scene中添加一个固定的Pill连接两个粒子
                 v_scene->addPill(id0, id1, true);
             }
         }
 
+        // 从一个名为"mesh.bin"的二进制文件中加载一个SurfaceMesh
         {
             SurfaceMesh tempmesh;
             std::ifstream istream("mesh.bin", std::ios::binary);
 
+            // 以下四个向量用于存储从文件中读取的网格数据
+            // 从文件中读取各个向量的数据
             std::vector<Vec3> pointvec(24842);
             istream.read(reinterpret_cast<char*>(&pointvec[0]), 24842 * sizeof(Vec3));
             
@@ -347,40 +389,50 @@ class OctopusComponent : public Component {
             std::vector<int> pidxvec(24842);
             istream.read(reinterpret_cast<char*>(&pidxvec[0]), 24842 * sizeof(int));
             
+            // 用于存储三角形索引的向量
             std::vector<int> trivec(12312 * 3);
             istream.read(reinterpret_cast<char*>(&trivec[0]), 12312 * 3 * sizeof(int));
 
+            // 使用从文件中读取的点数据向tempmesh添加顶点
             for (int i = 0; i < 24842; ++i) {
                 auto vert = tempmesh.add_vertex(pointvec[i]);
             }
 
+            // 添加各个顶点属性，如纹理坐标、法向量和pindex
             auto vtexcoord = tempmesh.add_vertex_property<Vec2>("v:texcoord");
             auto vnormal = tempmesh.add_vertex_property<Vec3>("v:normal");
             auto vpindex = tempmesh.add_vertex_property<int>("v:pindex");
 
+            // 使用从文件中读取的数据设置顶点属性
             for (int i = 0; i < 24842; ++i) {
                 SurfaceMesh::Vertex vert(i);
                 vnormal[vert] = normalvec[i];
                 vtexcoord[vert] = texvec[i];
                 vpindex[vert] = pidxvec[i];
             }
+
+            // 使用从文件中读取的索引数据向tempmesh添加三角形
             for (int i = 0; i < 12312; ++i) {
                 tempmesh.add_triangle(SurfaceMesh::Vertex(trivec[3*i]),
                                 SurfaceMesh::Vertex(trivec[3*i + 1]),
                                 SurfaceMesh::Vertex(trivec[3*i + 2]));
             }
 
+            // 使用从文件中读取的索引数据向tempmesh添加三角形
             mesh = tempmesh;
         }
 
+        // 使用cow模块的get_octopus方法获取章鱼的各种数据
         cow::get_octopus(spheres, all_pills, control_pills, masses,
                          compliances);
 
+        // 遍历spheres向量，并在smesh中为每个球体添加一个顶点
         for (int i = 0; i < spheres.size(); ++i) {
             auto &v = spheres[i];
             smesh.add_vertex(v);
         }
 
+        // 遍历control_pills，并为其中的每个药丸在smesh中添加一个边
         for (int i : control_pills) {
             auto pill = all_pills[i];
             smesh.add_edge(V(pill[0]), V(pill[1]));
@@ -388,10 +440,13 @@ class OctopusComponent : public Component {
 
         std::string output;
 
+        // 从mesh获取名为"v:pindex"的顶点属性
         auto vpindex = mesh.get_vertex_property<int>("v:pindex");
+        
+        // 创建一个映射，将每个顶点的索引映射到一个连续的顶点数组
         std::map<int, int> ind_verts;
         {
-            // build watertight mesh
+            // build watertight mesh 构建一个密闭的网格
             std::vector<Vec3> verts;
             for (auto vert : mesh.vertices()) {
                 int ind = vpindex[vert];
@@ -401,6 +456,7 @@ class OctopusComponent : public Component {
                 }
             }
 
+            // 将面片数据转换为三角形数据
             std::vector<Vec3i> tris;
             for (auto face : mesh.faces()) {
                 Vec3i tri;
@@ -410,18 +466,20 @@ class OctopusComponent : public Component {
                 tris.push_back(tri);
             }
 
-            // compute weights
+            // compute weights 使用rapidjson计算权重
 
             rapidjson::MemoryPoolAllocator<> alloc;
             rapidjson::Document j(&alloc);
             j.SetObject();
             rapidjson::Document::AllocatorType &allocator = j.GetAllocator();
 
+            // 将顶点和三角形数据添加到JSON文档中
             j.AddMember("vertices", viper::to_json(verts, allocator),
                         allocator);
             j.AddMember("triangles", viper::to_json(tris, allocator),
                         allocator);
 
+            // 为JSON文档创建一个球体和药丸的数组
             rapidjson::Value spheres_array(rapidjson::kArrayType);
             rapidjson::Value pills_array(rapidjson::kArrayType);
 
@@ -437,11 +495,13 @@ class OctopusComponent : public Component {
                     allocator),
                 allocator);
 
+            // 将JSON文档转换为字符串
             rapidjson::StringBuffer strbuf;
             strbuf.Clear();
             rapidjson::Writer<rapidjson::StringBuffer> writer(strbuf);
             j.Accept(writer);
 
+            // 使用viper模块运行"sphereweights"的外部进程，并传入上述字符串
             int proc_return =
                 viper::run_process("sphereweights", strbuf.GetString(), output);
             std::cout << "Sphereweights exited with code: " << proc_return
@@ -449,9 +509,11 @@ class OctopusComponent : public Component {
         }
 
         {
+            // 使用rapidjson解析上一个代码段中得到的JSON输出
             rapidjson::Document j;
             j.Parse(output.c_str());
 
+            // 从JSON中提取权重和骨骼ID信息
             auto all_weights =
                 viper::from_json<std::vector<std::vector<float>>>(j["weights"]);
             auto all_bones =
@@ -460,6 +522,7 @@ class OctopusComponent : public Component {
             std::vector<Vec4> weights(all_weights.size());
             std::vector<Vec4i> bones(all_bones.size());
 
+            // 格式化和归一化权重数据
             for (int i = 0; i < weights.size(); ++i) {
                 while (all_weights[i].size() < 4) {
                     all_weights[i].push_back(0.f);
@@ -473,6 +536,7 @@ class OctopusComponent : public Component {
                                  all_bones[i][2], all_bones[i][3]);
             }
 
+            // 将处理后的权重和骨骼ID存储到mesh中
             auto weights_prop = mesh.add_vertex_property<Vec4>("v:skinweight");
             auto bone_ids_prop = mesh.add_vertex_property<Vec4i>("v:boneid");
 
@@ -482,16 +546,19 @@ class OctopusComponent : public Component {
             }
         }
 
+        // 初始化储存id的向量
         v_ids.resize(n_cows);
         p_ids.resize(n_cows);
         cannonball_ids.resize(n_cows);
 
+        // 遍历所有的cow，对每个cow进行初始化
         for (int cow_id = 0; cow_id < n_cows; ++cow_id) {
 
             OctopusData data;
 
             Vec4 cannonball_s(0, 0, -1.5, 0.7);
 
+            // 初始化加农球并将其添加到场景中
             cannonball_ids[cow_id] = v_scene->addParticle(
                 cannonball_s.head<3>(), cannonball_s[3], 0.05);
             v_scene->pInfo[cannonball_ids[cow_id]].group = 1 + cow_id * 2;
@@ -499,6 +566,7 @@ class OctopusComponent : public Component {
             cannonball_smesh.add_sphere(
                 cannonball_smesh.add_vertex(cannonball_s));
 
+            // 遍历并添加spheres到场景
             for (int i = 0; i < spheres.size(); ++i) {
                 auto &v = spheres[i];
                 float w = 1.0 / masses[i];
@@ -506,11 +574,13 @@ class OctopusComponent : public Component {
                     v_scene->addParticle(v.head<3>(), v[3], w));
                 v_scene->pInfo[v_ids[cow_id].back()].group = cow_id * 2;
 
+                // 添加约束条件
                 data.radius_constraints.push_back(
                     v_scene->constraints.radius.size());
                 v_scene->constraints.radius.push_back(
                     viper::C_radius(v_ids[cow_id].back(), v[3], 1e-3));
 
+                // 对于特定的球体，添加与加农球的距离约束
                 if (i >= 10 && i < 18) {
 
                     data.cannonball_constraints.push_back(
@@ -521,6 +591,7 @@ class OctopusComponent : public Component {
                 }
             }
 
+            // 遍历并添加pills到场景，并添加相关约束条件
             for (int i = 0; i < all_pills.size(); i++) {
                 auto pill = all_pills[i];
                 int p_id = v_scene->addPill(v_ids[cow_id][pill[0]],
@@ -541,6 +612,8 @@ class OctopusComponent : public Component {
                     v_ids[cow_id][pill[0]], v_ids[cow_id][pill[1]], p_id, d,
                     compliance));
             }
+
+            // 对于所有pills，检查它们之间是否存在连接，并相应地添加弯曲约束
             for (int i = 0; i < all_pills.size(); ++i) {
                 for (int j = i + 1; j < all_pills.size(); ++j) {
                     auto pill_i = all_pills[i];
@@ -558,16 +631,20 @@ class OctopusComponent : public Component {
                 }
             }
 
+            // 将初始化的数据存储
             cow_data.push_back(data);
         }
 
+        // 获取并上传初始的变换
         init_transforms = get_transforms();
         renderer->upload_mesh(mesh, get_transforms());
         renderer->get_gpu_mesh().set_vtexcoord(
             mesh.get_vertex_property<Vec2>("v:texcoord").vector());
 
+        // 重置场景状态
         reset();
 
+        // 清除smesh并重新构建
         smesh.clear();
 
         int offset = 0;
@@ -584,6 +661,7 @@ class OctopusComponent : public Component {
             offset += spheres.size();
         }
 
+        // 再次重置场景状态
         reset();
     }
 
